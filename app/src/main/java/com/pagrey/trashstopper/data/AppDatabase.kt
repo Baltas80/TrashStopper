c78f0a1d708d4ae3e6c4bdc9692cb90b986667f2
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-11 14:30:26 +0200

    feat(data): add room database

---
+++ app/src/main/java/com/pagrey/trashstopper/data/AppDatabase.kt
@@ -0,0 +1,24 @@
+package com.pagrey.trashstopper.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = [NumberEntity::class], version = 1, exportSchema = false)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun numberDao(): NumberDao
+
+    companion object {
+        @Volatile private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase =
+            INSTANCE ?: synchronized(this) {
+                INSTANCE ?: Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "trashstopper.db"
+                ).build().also { INSTANCE = it }
+            }
+    }
+}
