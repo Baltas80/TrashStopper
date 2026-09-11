3f413e3c6e9a3723284b8b12fdfc94b65d673ef4
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-11 19:02:23 +0200

    feat(protection): expose runtime policy

--- app/src/main/java/com/pagrey/trashstopper/screening/ScreeningRuntime.kt
+++ app/src/main/java/com/pagrey/trashstopper/screening/ScreeningRuntime.kt
@@ -3,4 +3,5 @@ package com.pagrey.trashstopper.screening
 object ScreeningRuntime {
     val cache: LocalReputationCache = LocalReputationCache()
     val rules: LocalRuleCache = LocalRuleCache()
+    val protection: ProtectionPolicy = ProtectionPolicy()
 }
