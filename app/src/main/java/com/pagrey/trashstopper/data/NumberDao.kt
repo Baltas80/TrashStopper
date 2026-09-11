c964d68344fb01d9f858720906674ae6fb17ace6
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-11 13:26:13 +0200

    data: add number dao

---
+++ app/src/main/java/com/pagrey/trashstopper/data/NumberDao.kt
@@ -0,0 +1,18 @@
+package com.pagrey.trashstopper.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NumberDao {
+    @Query("SELECT * FROM numbers WHERE phoneNumber = :phoneNumber LIMIT 1")
+    suspend fun find(phoneNumber: String): NumberEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(number: NumberEntity)
+
+    @Query("DELETE FROM numbers")
+    suspend fun clear()
+}
