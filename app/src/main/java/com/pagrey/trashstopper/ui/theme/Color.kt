728f1fddce414673cdbfc6271d1672b7c9178fcc
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-11 11:52:43 +0200

    ui: add Trash Stopper brand and semantic colors

---
+++ app/src/main/java/com/pagrey/trashstopper/ui/theme/Color.kt
@@ -0,0 +1,30 @@
+package com.pagrey.trashstopper.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Trash Stopper brand palette. Semantic risk colors are supplementary; UI must
+// always pair risk with text/iconography as defined by the design system.
+val TrashPrimaryLight = Color(0xFF315BFF)
+val TrashOnPrimaryLight = Color(0xFFFFFFFF)
+val TrashPrimaryContainerLight = Color(0xFFDDE4FF)
+val TrashOnPrimaryContainerLight = Color(0xFF07164D)
+val TrashBackgroundLight = Color(0xFFF8F9FC)
+val TrashSurfaceLight = Color(0xFFFFFFFF)
+val TrashSurfaceVariantLight = Color(0xFFE9ECF4)
+val TrashOnSurfaceLight = Color(0xFF171A22)
+val TrashOnSurfaceVariantLight = Color(0xFF454954)
+
+val TrashPrimaryDark = Color(0xFFB7C4FF)
+val TrashOnPrimaryDark = Color(0xFF102A8B)
+val TrashPrimaryContainerDark = Color(0xFF203C9C)
+val TrashOnPrimaryContainerDark = Color(0xFFDDE4FF)
+val TrashBackgroundDark = Color(0xFF101217)
+val TrashSurfaceDark = Color(0xFF17191F)
+val TrashSurfaceVariantDark = Color(0xFF292C34)
+val TrashOnSurfaceDark = Color(0xFFE4E2EA)
+val TrashOnSurfaceVariantDark = Color(0xFFC4C5CE)
+
+val RiskSafe = Color(0xFF167C4A)
+val RiskCaution = Color(0xFF8A5A00)
+val RiskHigh = Color(0xFFB54708)
+val RiskCritical = Color(0xFFB42318)
