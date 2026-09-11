fefc0bb16d0474d2490b153e9759c80c8b45d5ee
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-11 16:50:08 +0200

    fix(ui): import rememberSaveable explicitly

--- app/src/main/java/com/pagrey/trashstopper/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/pagrey/trashstopper/ui/screens/search/SearchScreen.kt
@@ -4,7 +4,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Search
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.pagrey.trashstopper.data.NumberEntity
