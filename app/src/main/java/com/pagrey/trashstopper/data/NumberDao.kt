7ebb7d36c9df9ced5d6a91e2019926577469e5fd
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-11 14:30:21 +0200

    feat(data): add number reputation dao

---
+++ app/src/main/java/com/pagrey/trashstopper/data/NumberDao.kt
@@ -0,0 +1,21 @@
+package com.pagrey.trashstopper.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface NumberDao {
+    @Query("SELECT * FROM numbers WHERE phoneNumber = :phoneNumber LIMIT 1")
+    suspend fun find(phoneNumber: String): NumberEntity?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(number: NumberEntity)
+
+    @Query("SELECT * FROM numbers")
+    suspend fun getAll(): List<NumberEntity>
+
+    @Query("DELETE FROM numbers")
+    suspend fun clear()
+}
